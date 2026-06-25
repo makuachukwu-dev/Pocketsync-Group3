12941596881fc508868acd589f1faa174d6cd242
Makuachukwu Iloabachie <iloabachiem@gmail.com>
2026-06-25 20:50:33 +0100

    implement onboarding flow and login UI

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,13 @@
+
+// splash scren
+if (window.location.pathname.includes("index.html")) {
+
+    setTimeout(() => {
+        window.location.href = "onboarding-1.html";
+    }, 3000);
+
+}
+//onboarding flow
 function goNext() {
      window.location.href = "onboarding-2.html";
 }
@@ -9,3 +19,40 @@ function goToOnboarding3() {
 function goToSignup() {
     window.location.href = "signup.html";
 }
+
+
+
+//log in page
+const passwordInput = document.getElementById("password");
+const togglePassword = document.querySelector(".toggle-password");
+
+togglePassword.addEventListener("click", function () {
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePassword.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        togglePassword.textContent = "Show";
+    }
+
+});
+
+//validation for login
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (email === "" || password === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    alert("Login successful!");
+
+});
