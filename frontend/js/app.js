ae56f91a44fa058af802ac74fdc5c58e68dff7e9
Makuachukwu Iloabachie <iloabachiem@gmail.com>
2026-06-25 17:02:32 +0100

    Implement splash screen and onboarding flow

---
+++ frontend/js/app.js
@@ -0,0 +1,11 @@
+function goNext() {
+     window.location.href = "onboarding-2.html";
+}
+
+function goToOnboarding3() {
+     window.location.href = "onboarding-3.html";
+}
+
+function goToSignup() {
+    window.location.href = "signup.html";
+}
